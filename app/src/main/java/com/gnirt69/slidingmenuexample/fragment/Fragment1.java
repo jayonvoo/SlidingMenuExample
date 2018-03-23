2d4b0697e9b6fca71645983313e4ff800db9b1a2
jayonvoo <kiyoto_zy@hotmail.com>
2018-03-23 14:19:31 +0800

    update 0940

--- app/src/main/java/com/gnirt69/slidingmenuexample/fragment/Fragment1.java
+++ app/src/main/java/com/gnirt69/slidingmenuexample/fragment/Fragment1.java
@@ -18,16 +18,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
 import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.gnirt69.slidingmenuexample.Database.DBAction;
 import com.gnirt69.slidingmenuexample.R;
 
-import java.util.ArrayList;
+import static java.lang.System.out;
 
 public class Fragment1 extends Fragment {
 
@@ -35,14 +35,13 @@ public class Fragment1 extends Fragment {
     private TextView textView, countDown;
     private EditText editText;
     private String getText;
-    private ListView listView;
+    private SwipeMenuListView listView;
     private DBAction linkData;
     private Cursor getData;
     private int additem;
     private ListAdapter defaultAdapter;
     private int numOfTask = 0;
     private SwipeMenuCreator creator;
-
 
     public Fragment1() {
 
@@ -61,45 +60,8 @@ public class Fragment1 extends Fragment {
         button = (Button) rootView.findViewById(R.id.button1);
         delbutton = (Button) rootView.findViewById(R.id.button2);
         editText = (EditText) rootView.findViewById(R.id.editText1);
-        listView = (ListView) rootView.findViewById(R.id.listview01);
+        listView = (SwipeMenuListView) rootView.findViewById(R.id.listview01);
         countDown = (TextView) rootView.findViewById(R.id.countdown);
-
-
-        creator = new SwipeMenuCreator() {
-
-            @Override
-            public void create(SwipeMenu menu) {
-                // create "open" item
-                SwipeMenuItem openItem = new SwipeMenuItem(
-                        getActivity());
-                // set item background
-                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
-                        0xCE)));
-                // set item width
-                openItem.setWidth(90);
-                // set item title
-                openItem.setTitle("Open");
-                // set item title fontsize
-                openItem.setTitleSize(18);
-                // set item title font color
-                openItem.setTitleColor(Color.WHITE);
-                // add to menu
-                menu.addMenuItem(openItem);
-
-                // create "delete" item
-                SwipeMenuItem deleteItem = new SwipeMenuItem(
-                        getActivity());
-                // set item background
-                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
-                        0x3F, 0x25)));
-                // set item width
-                deleteItem.setWidth(90);
-                // set a icon
-                deleteItem.setIcon(R.drawable.ic_delete);
-                // add to menu
-                menu.addMenuItem(deleteItem);
-            }
-        };
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,10 +92,13 @@ public class Fragment1 extends Fragment {
 
                 //將資料更新或顯示到View上
                 defaultAdapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_list_item_1, linkData.GetAllData());
+                initializeList();
                 listView.setAdapter(defaultAdapter);
 
             }
+
         });
+
 
         delbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -141,6 +106,7 @@ public class Fragment1 extends Fragment {
                 linkData.DeleteTable();
 
                 defaultAdapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_list_item_1, linkData.GetAllData());
+                initializeList();
                 listView.setAdapter(defaultAdapter);
             }
         });
@@ -154,8 +120,32 @@ public class Fragment1 extends Fragment {
         });
 
         defaultAdapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_list_item_1, linkData.GetAllData());
+        initializeList();
         listView.setAdapter(defaultAdapter);
 
         return rootView;
+    }
+
+    private void initializeList() {
+        creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getActivity());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(90);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        listView.setMenuCreator(creator);
     }
 }
